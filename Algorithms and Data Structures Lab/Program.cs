bf05f7deef6e4b2450f5d16e8e2fecd6cdecaf39
Yuhan Zhao <yuhanzhao@student.mitt.ca>
2023-06-01 09:10:26 -0500

    update

--- Algorithms and Data Structures Lab/Program.cs
+++ Algorithms and Data Structures Lab/Program.cs
@@ -5,25 +5,68 @@ public sealed class Lab02
 {
     static void Main()
     {
-        string test = "Programmatic Pythonasrbsn gggggggg43563 gg";
+        string example1 = "Programmatic Python";
         int count = 0;
-        string words = "";
-        for (int i = 0; i < test.Length; i++)
+        string example1Words = "";
+        for (int i = 0; i < example1.Length; i++)
         {
-            for (int j = i + 1; j < test.Length; j++)
+            for (int j = i + 1; j < example1.Length; j++)
             {
-                if (test[i] == test[j] && Char.IsLetter(test[i]) && test[i] != ' ' && !words.Contains(test[i]))
+                if (example1[i] == example1[j] && Char.IsLetter(example1[i]) && example1[i] != ' ' && !example1Words.Contains(example1[i]))
                 {
                     count++;
-                    words += test[j];
+                    example1Words += example1[j];
                 }
             }
         }
         StringBuilder savedArray = new StringBuilder("", count);
         for (int i = 0; i < count; i++)
         {
-            savedArray.Append(words[i]);
+            savedArray.Append(example1Words[i]);
         }
-        Console.WriteLine(savedArray);
+        Console.WriteLine("[" + string.Join(",", savedArray) + "]");
+
+        string input = "Programmatic Python";
+        List<char> result = new List<char>();
+
+        foreach (char c in input.ToLower())
+        {
+            if (Char.IsLower(c) && !result.Contains(c))
+            {
+                result.Add(c);
+            }
+        }
+
+        char[] arrayResult = result.ToArray();
+
+        Console.WriteLine("[\"" + string.Join("\", \"", result) + "\"]");
+
+        string typeString = "in";
+        string example2 = "Insured in that field";
+        int len = 0;
+        string words = "";
+        int temp = 0;
+        for (int i = 0; i < example2.Length; i++)
+        {
+            len++;
+            if (example2[i] == ' ')
+            {
+                if (len - 1 == typeString.Length)
+                {
+                    words = example2.Substring(temp, len - 1);
+                    Console.WriteLine(words);
+                    if (words == typeString)
+                    {
+                        words = example2.Substring(temp, example2.Length - i + 2);
+                    }
+                }
+                else
+                {
+                    temp = i + 1;
+                    len = 0;
+                }
+            }
+        }
+        Console.WriteLine(words);
     }
 }
