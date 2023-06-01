470680cddcac4224c96fb032b4e4efdce34ed783
Yuhan Zhao <yuhanzhao@student.mitt.ca>
2023-05-31 23:28:27 -0500

    update

--- Algorithms and Data Structures Lab/Program.cs
+++ Algorithms and Data Structures Lab/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+public sealed class Lab02
+{
+    static void Main()
+    {
+        string test = "Programmatic Python";
+        int count = 0;
+        string words = "";
+        for (int i = 0; i < test.Length; i++)
+        {
+            for (int j = i + 1; j < test.Length; j++)
+            {
+                if (test[i] == test[j])
+                {
+                    count++;
+                    words += test[j];
+                }
+            }
+        }
+        StringBuilder savedArray = new StringBuilder("", count);
+        for (int i = 0; i < count; i++)
+        {
+            savedArray.Append(words[i]);
+        }
+        Console.WriteLine(savedArray);
+    }
+}
